15378d4dc8090145197a6fbcb7bab03baec3e75d
Omar Said Kamel Ahmed <75030145+omarsaidkamel@users.noreply.github.com>
2024-11-01 14:08:52 +0200

    Create FirstQuizLecture2.cpp

---
+++ FirstQuizLecture2.cpp
@@ -0,0 +1,24 @@
+/*
+Q1
+1-        *    
+      *       *
+          *
+      *       *
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<" *\n*  *\n *\n*  *"
+  return 0;
+}
+
+/*
+Q2
+2- Print this sentence “ Hello \ “OI CPC” \  “.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"\" Hello \\ \"OI CPC\" \\  \""
+  return 0;
+}
